1c2e5f7c9c989fe453078b551b8c3c27ee5614ef
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-04-03 13:41:41 -0700

    uploading for completed flask assignment

--- flask/flask_fundamentals/understanding_routing/assignment.py
+++ flask/flask_fundamentals/understanding_routing/assignment.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask 
 app = Flask(__name__)
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route('/')          
+def hello_world():
+    return "Hello World!"
 
-# @app.route("/")
-# def hello_world():
-#     return "Hello World!"
+@app.route("/dojo")
+def success():
+    return "Dojo!"
 
-# @app.route("/dojo")
-# def dojo():
-#     return "Dojo!"
+@app.route("/say/<name>")
+def show_user_profile(name):
+    return "Hi "+ name + "!"
 
-# @app.route("/say/<name>")
-# def show_name(name):
-#     return "Hi " + name + "!"
+@app.route("/repeat/<num>/<word>")
+def repeat_after_me(num,word):
+    var = int(num)
+    my_str = ""
+    for i in range(var):
+        my_str += f"{word} \n"
+    return my_str
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
---
+++ python/fundamentals/testing_stuff.py
@@ -0,0 +1,7 @@
+def integer_or_string(something):
+    if (num.isdigit() == True):
+        print("yay")
+    else:
+        print("shit")
+
+integer_or_string(45)
