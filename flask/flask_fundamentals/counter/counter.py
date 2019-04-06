28358773590d14f38db30887a3a11f76f5d9d0ce
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-04-05 22:24:36 -0700

    uploading mostly completed great number game files

--- flask/flask_fundamentals/counter/counter.py
+++ flask/flask_fundamentals/counter/counter.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-app.secret_key:"trying something out"
-app.count = 0
+app.secret_key="trying something out"
 
 @app.route('/')
 def index_count():
-    session['count'] += 1
-    return render_template('index.html', count=session['count'])
+    if 'visits' in session:
+        session['visits'] += 1
+    else:
+        session['visits'] = 1
+    return render_template('index.html', count=session['visits'])
 
 @app.route('/destroy', methods=['POST'])
 def destroy_count():
-    session['count'] = 0
+    session.pop('visits')
     return redirect('/')
 
 if __name__ == "__main__":
---
+++ flask/flask_fundamentals/great_number_game/great_number_game.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "TopSecret"
+
+@app.route('/')
+def homepage():
+    if "guess" not in session:
+        import random
+        random_num = random.randint(1,100)
+        print("*"*50)
+        print("The cache has been cleared.")
+        print(f"Our random number is {random_num}")
+        session['random_num'] = random_num
+        session['phrase']=""
+        session['color']=""
+        print("*"*50)
+    else:
+        goal_num = int(session['random_num'])
+        guess_num = int(session['guess'])
+        print(f"The value of our guess is:{guess_num}. The value of our goal number is {goal_num}")
+        if guess_num == goal_num:
+            print("*"*50)
+            print("You're so totally correct!!")
+            session['phrase']=f"{goal_num} was the number!"
+            print(session['phrase'])
+            session['color']=f"green"
+            print("*"*50)
+        elif guess_num < goal_num:
+            print("*"*50)
+            print("the number is too low")
+            session['phrase']=f"Too low!"
+            session['color']=f"red"
+            print(session['phrase'])
+            print("*"*50)
+        elif guess_num > goal_num:
+            print("*"*50)
+            print("the number is too high")
+            session['phrase']=f"Too high!"
+            print(session['phrase'])
+            session['color']=f"red"
+            print("*"*50)
+    return render_template('index.html',
+        sentence=session['phrase'],
+        bgcolor=session['color'])
+
+@app.route('/process', methods=['POST'])
+def process_form():
+    session['guess']=request.form['guess']
+    return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset_numbers():
+    if "guess" in session:
+        session.pop('guess')
+        session.pop('phrase')
+        session.pop('color')
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
