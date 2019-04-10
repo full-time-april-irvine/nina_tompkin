23d90d92020a71b55a2e2658004561bb5d17fe57
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-04-10 11:28:35 -0700

    finished login and reg

--- flask/flask_mysql/login_and_reg/server.py
+++ flask/flask_mysql/login_and_reg/server.py
@@ -23,6 +23,18 @@ def process_user():
     if not EMAIL_REGEX.match(request.form["email"]):
         flash("Invalid email address.")
         is_valid = False
+    mysql = connectToMySQL("login_and_reg")
+    query = "SELECT * FROM users WHERE email = %(em)s;"
+    data = {
+        "em":request.form["email"]
+        }
+    matching_users = mysql.query_db(query,data)
+    print("*"*50)
+    print(matching_users)
+    print("*"*50)
+    if matching_users:
+        flash("Email already in use")
+        is_valid = False
     if(len(request.form["password1"])) < 8:
         flash("Password must contain a number, a capital letter, and be between 8-15 characters.")
         is_valid = False
@@ -36,9 +48,6 @@ def process_user():
         return redirect("/")
     else:
         pw_hash = bcrypt.generate_password_hash(request.form["password1"])
-        print("*"*50)
-        print(pw_hash)
-        print("*"*50)
         mysql = connectToMySQL("login_and_reg")
         query = "INSERT INTO users (first_name, last_name, email, password_hash, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(em)s, %(password_hash)s, NOW(), NOW());"
         data = {
@@ -47,29 +56,35 @@ def process_user():
             "em":request.form["email"],
             "password_hash":pw_hash 
             }
-        new_user = mysql.query_db(query,data)
-        print("*"*50)
-        print(new_user)
-        print("*"*50)
-        return redirect("/success")
-
-@app.route("/success")
-def success():
-    print("Hello world")
-    return render_template("success.html")
+        user_id = mysql.query_db(query,data)
+        session["userid"] = user_id
+        return redirect("/")
 
 @app.route("/login", methods=["POST"])
 def login():
     mysql = connectToMySQL("login_and_reg")
     query = "SELECT * FROM users WHERE email = %(em)s;"
     data = { "em":request.form["email"] }
-    result = mysql.query_db(query,data)
-    if len(result) > 0:
-        if bcrypt.check_password_hash(result[0]["password"],request.form["password1"]):
-            session["userid"] = result[0]["id"]
+    matching_users = mysql.query_db(query,data)
+    if matching_users:
+        user = matching_users[0]
+        if bcrypt.check_password_hash(user["password_hash"],request.form["password1"]):
+            session["userid"] = user["id"]
             return redirect("/success")
+
     flash("You could not be logged in")
     return redirect("/")
+
+@app.route("/success")
+def success():
+    mysql = connectToMySQL("login_and_reg")
+    query = "SELECT * FROM users WHERE id = %(recent_id)s;"
+    data = {
+        "recent_id":session["userid"]
+    }
+    recent_user = mysql.query_db(query,data)
+    return render_template("success.html",our_user=recent_user)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
